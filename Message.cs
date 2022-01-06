afaee0f614e2d26df9ba9b6e53521d3f52b088bc
Ibryam <57998847+Ibryam512@users.noreply.github.com>
2022-01-06 08:34:46 +0200

    Add Message functionallity

---
+++ Message.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AntimalnikAPI.Models
+{
+    public class Message
+    {
+        public ApplicationUser Sender { get; set; }
+        public ApplicationUser Reciever { get; set; }
+        public string Text { get; set; }
+    }
+}
